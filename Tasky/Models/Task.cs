b4cab1e9481f6b3dac455f8b1173c377b465b1a9
Black <Black@DESKTOP-PCSEWR>
2024-05-31 09:15:17 +0100

    Add project files.

---
+++ Tasky/Models/Task.cs
@@ -0,0 +1,31 @@
+﻿namespace Tasky.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime Updated { get; set; }   
+
+        public User CreatedBy { get; set; }
+
+        public  ICollection<User> AssignedTo { get; set; }
+
+        public STATUS Status {  get; set; }
+
+        public int Rating { get; set; }
+
+    }
+}
+
+public enum STATUS { 
+    TODO,
+    IN_PROGRESS,
+    COMPLETED
+}
+
+
