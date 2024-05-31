24ff89890a3045898e41f8e746383c70f1e148bb
John Adeniran <jasolajohn@gmail.com>
2024-05-31 09:17:36 +0100

    Created models

--- Tasky/Models/User.cs
+++ Tasky/Models/User.cs
@@ -7,7 +7,17 @@
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string UserName { get; set; }
+        public string UserName { get; set; } 
+
+        public ROLE Role { get; set; }
 
     }
+}
+
+
+public enum ROLE
+{
+    USER,
+    MANAGER,
+    ADMIN
 }
